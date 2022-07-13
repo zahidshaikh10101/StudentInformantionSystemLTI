568b310b342fe3d77c1d809e7e4a4407eff70cc0
Zahid Shaikh <zahidshaikh10101@gmail.com>
2022-07-13 13:06:23 +0530

    New commit

--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -3,5 +3,29 @@ package model;
 public class Customer {
 	int customerId;
 	String customerName;
+	
+	public Customer(String customerName) {
+		super();
+		this.customerName = customerName;
+	}
+
+	public Customer(int customerId) {
+		super();
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	
+	
+	
+	
+	
 
 }
